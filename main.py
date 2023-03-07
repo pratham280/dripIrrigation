14bae788fc9f7c69f746c9d32065de9ee46da4cf
Prathamesh <prathameshmawale@gmail.com>
2023-03-07 20:33:33 +0530

    added more functionality like is manual button along side soil sensor

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ while True:
     seconds = time.localtime(time.time() + offset)[5]
     realtime = hours + minutes + seconds
 
-    if soil.read() >= 0 and soil.read() <= 750:
+    if (soil.read() >= 0 and soil.read() <= 750) and watering == False:
         if relay.value() == 1:
             revert()
         else:
             pass
-    elif soil.read() > 750:
+    elif (soil.read() > 750) and watering == False:
         if realtime >= 34680 and realtime <= 35400:
             if (stop.value() == 1 and fail == False):
                 revert()
@@ -28,8 +28,11 @@ while True:
             fail = False
             watering = False
             revert()
-        elif realtime >= 35400 and fail == False:
-            if button_on.value() == 1:
-                water()
-            elif stop.value() == 1:
-                revert()
+        else:
+            pass
+    if (button_on.value() == 1) and watering == False:
+        water()
+        watering = True
+    elif (stop.value() == 1) and watering == True:
+        revert()
+        watering = False
